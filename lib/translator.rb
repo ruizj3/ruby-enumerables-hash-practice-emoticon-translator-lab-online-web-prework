e3ab26caf7d035ec43636ef72e21a31f4653f9de
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-16 01:29:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -4,15 +4,15 @@ require "yaml"
 def load_libraryz(file)
   # code goes here
   emoticons = YAML.load_file(file)
-
+  
   new_hash = {
-    :get_meaning => {},
-    :get_emoticon => {}
+    'get_meaning' => {},
+    'get_emoticon' => {}
   }
 
   emoticons.each do |key,value|
-    new_hash[:get_emoticon][value[0]] = emoticons[key][1]
-    new_hash[:get_meaning][value[1]] = key
+    new_hash['get_emoticon'][value[0]] = emoticons[key][1]
+    new_hash['get_meaning'][value[1]] = key
   end
 
   new_hash
