fa9a6706a8dc5265eebdff2f5250d08dc1bcd3f6
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-16 01:37:15 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -17,8 +17,10 @@ def load_library(file_path)
   new_hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file,emoticon)
   # code goes here
+  result = load_library(file)['get_emoticon'][emoticon]
+  result ? result : "null"
 end
 
 def get_english_meaning
