1f27f25fdc796c5d7f36b46b6c9f69c06ec81e12
cool-changelog-3932 <ruizj3@uw.edu>
2019-10-16 01:41:48 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,30 @@
 # require modules here
+require "yaml"
+# code goes here
+def load_library(file_path)
+  emoticons = YAML.load_file(file_path)
 
-def load_library
-  # code goes here
+  new_hash = {
+    'get_meaning' => {},
+    'get_emoticon' => {}
+  }
+
+  emoticons.each do |key,value|
+    new_hash['get_emoticon'][value[0]] = emoticons[key][1]
+    new_hash['get_meaning'][value[1]] = key
+  end
+
+  new_hash
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(file,emoticon)
   # code goes here
+  translations = load_library(file)['get_emoticon'][emoticon]
+  translations ? translations : "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
+def get_english_meaning(file,emoticon)
   # code goes here
+  translations = load_library(file)['get_meaning'][emoticon]
+  translations ? translations : "Sorry, that emoticon was not found"
 end
